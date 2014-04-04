f7f715ec438ad5d80b2815c9ce77112d8823868c
I-Hara <i.hara.224@gmail.com>
2014-04-05 03:37:36 +0900

    Initial Commit

---
+++ TestSingleView/TestSingleView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSingleView
+//
+//  Created by Isao HARA on 2014/04/05.
+//  Copyright (c) 2014年 IsaoHARA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestSingleView/TestSingleView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestSingleView
+//
+//  Created by Isao HARA on 2014/04/05.
+//  Copyright (c) 2014年 IsaoHARA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
